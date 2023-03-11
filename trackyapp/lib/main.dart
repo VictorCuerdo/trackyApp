1c7a2981279a0f1c999ec4c1ec6e8584d2b50891
Victor Cuerdo <vcuerdo@uninorte.edu.co>
2023-03-11 13:51:17 -0500

    User form design updated
    Se cambio el diseño del user form, añadiendo un cuadrado blanco para mejorar el diseño, también se sacó la camara del appbar y se puso en mi widget.

--- trackyapp/lib/main.dart
+++ trackyapp/lib/main.dart
@@ -30,95 +30,108 @@ class _EditPersonalInfoState extends State<EditPersonalInfo> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Edit Personal Information'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.photo_camera),
-            onPressed: () {
-              // TODO: implement photo upload
-            },
-          ),
-        ],
       ),
-      body: Container(
-        color: Colors.blue,
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(16.0),
+      body: SingleChildScrollView(
+        child: Container(
+          color: Colors.blue,
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              SizedBox(height: 16.0),
+              Container(
+                alignment: Alignment.topRight,
+                child: Container(
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.white,
+                  ),
+                  child: IconButton(
+                    icon: Icon(Icons.photo_camera),
+                    color: Colors.blue,
+                    onPressed: () {
+                      // TODO: implement photo upload
+                    },
+                  ),
+                ),
               ),
-              padding: EdgeInsets.all(16.0),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  TextFormField(
-                    decoration: InputDecoration(labelText: 'Name'),
-                    initialValue: _name,
-                    onChanged: (value) {
-                      setState(() {
-                        _name = value;
-                      });
-                    },
-                  ),
-                  SizedBox(height: 16.0),
-                  TextFormField(
-                    decoration: InputDecoration(labelText: 'Last Name'),
-                    initialValue: _lastName,
-                    onChanged: (value) {
-                      setState(() {
-                        _lastName = value;
-                      });
-                    },
-                  ),
-                  SizedBox(height: 16.0),
-                  TextFormField(
-                    decoration: InputDecoration(labelText: 'Email'),
-                    initialValue: _email,
-                    onChanged: (value) {
-                      setState(() {
-                        _email = value;
-                      });
-                    },
-                  ),
-                  SizedBox(height: 16.0),
-                  TextFormField(
-                    decoration: InputDecoration(labelText: 'Telephone'),
-                    initialValue: _telephone,
-                    onChanged: (value) {
-                      setState(() {
-                        _telephone = value;
-                      });
-                    },
-                  ),
-                  SizedBox(height: 16.0),
-                  TextFormField(
-                    decoration: InputDecoration(labelText: 'Weight'),
-                    initialValue: _weight?.toString(),
-                    keyboardType:
-                        TextInputType.numberWithOptions(decimal: true),
-                    onChanged: (value) {
-                      setState(() {
-                        _weight = double.tryParse(value);
-                      });
-                    },
-                  ),
-                  SizedBox(height: 16.0),
-                  TextFormField(
-                    decoration: InputDecoration(labelText: 'Height'),
-                    initialValue: _height?.toString(),
-                    keyboardType:
-                        TextInputType.numberWithOptions(decimal: true),
-                    onChanged: (value) {
-                      setState(() {
-                        _height = double.tryParse(value);
-                      });
-                    },
-                  ),
-                ],
+              SizedBox(height: 16.0),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(16.0),
+                ),
+                padding: EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    TextFormField(
+                      decoration: InputDecoration(labelText: 'Name'),
+                      initialValue: _name,
+                      onChanged: (value) {
+                        setState(() {
+                          _name = value;
+                        });
+                      },
+                    ),
+                    SizedBox(height: 16.0),
+                    TextFormField(
+                      decoration: InputDecoration(labelText: 'Last Name'),
+                      initialValue: _lastName,
+                      onChanged: (value) {
+                        setState(() {
+                          _lastName = value;
+                        });
+                      },
+                    ),
+                    SizedBox(height: 16.0),
+                    TextFormField(
+                      decoration: InputDecoration(labelText: 'Email'),
+                      initialValue: _email,
+                      onChanged: (value) {
+                        setState(() {
+                          _email = value;
+                        });
+                      },
+                    ),
+                    SizedBox(height: 16.0),
+                    TextFormField(
+                      decoration: InputDecoration(labelText: 'Telephone'),
+                      initialValue: _telephone,
+                      onChanged: (value) {
+                        setState(() {
+                          _telephone = value;
+                        });
+                      },
+                    ),
+                    SizedBox(height: 16.0),
+                    TextFormField(
+                      decoration: InputDecoration(labelText: 'Weight'),
+                      initialValue: _weight?.toString(),
+                      keyboardType:
+                          TextInputType.numberWithOptions(decimal: true),
+                      onChanged: (value) {
+                        setState(() {
+                          _weight = double.tryParse(value);
+                        });
+                      },
+                    ),
+                    SizedBox(height: 16.0),
+                    TextFormField(
+                      decoration: InputDecoration(labelText: 'Height'),
+                      initialValue: _height?.toString(),
+                      keyboardType:
+                          TextInputType.numberWithOptions(decimal: true),
+                      onChanged: (value) {
+                        setState(() {
+                          _height = double.tryParse(value);
+                        });
+                      },
+                    ),
+                  ],
+                ),
               ),
-            ),
+            ],
           ),
         ),
       ),
