007599c7dd9e57ad00687a1d3bc7a5d9bf631a5a
Victor Cuerdo <vcuerdo@uninorte.edu.co>
2023-03-11 18:47:18 -0500

    exit icon added
    pocos cambios y se agrega el botón de exit

--- trackyapp/lib/main.dart
+++ trackyapp/lib/main.dart
@@ -32,142 +32,158 @@ class _EditPersonalInfoState extends State<EditPersonalInfo> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-          color: Colors.blue,
-          padding: const EdgeInsets.all(
-              35.0), // ajusta los bordes laterale del formulario. Establece que tan ancho es el formulario
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              const SizedBox(height: 30.0),
-              Row(
-                children: const [
-                  Padding(
-                    padding: EdgeInsets.only(left: 10.0),
-                    child: Text(
-                      'Tania',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 80.0,
-                        fontWeight: FontWeight.bold,
-                        letterSpacing: -1.5,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  const Expanded(
-                    child: Padding(
-                      padding: EdgeInsets.only(left: 20.0, right: 20),
-                      child: Text(
-                        'Barranquilla, Atlantico',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 16.0,
-                          fontStyle: FontStyle.italic,
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            child: Container(
+              color: Colors.blue,
+              padding: const EdgeInsets.all(
+                  35.0), // ajusta los bordes laterale del formulario. Establece que tan ancho es el formulario
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  const SizedBox(height: 30.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.only(left: 10.0),
+                        child: Text(
+                          'Tania',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 80.0,
+                            fontWeight: FontWeight.bold,
+                            letterSpacing: -1.5,
+                          ),
                         ),
                       ),
-                    ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.exit_to_app, size: 40),
+                        color: Colors.white,
+                        padding: const EdgeInsets.only(top: 5.0, right: 10.0),
+                        // alignment: Alignment.topRight,
+                      ),
+                    ],
                   ),
+                  Row(
+                    children: [
+                      const Expanded(
+                        child: Padding(
+                          padding: EdgeInsets.only(left: 20.0, right: 20),
+                          child: Text(
+                            'Barranquilla, Atlantico',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16.0,
+                              fontStyle: FontStyle.italic,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        width: 130,
+                        height: 130,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.white,
+                        ),
+                        child: IconButton(
+                          icon: const Icon(Icons.photo_camera, size: 70),
+                          color: Colors.blue,
+                          onPressed: () {
+                            // TODO: implement photo upload
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 20.0),
                   Container(
-                    width: 130,
-                    height: 130,
-                    decoration: const BoxDecoration(
-                      shape: BoxShape.circle,
+                    decoration: BoxDecoration(
                       color: Colors.white,
+                      borderRadius: BorderRadius.circular(16.0),
                     ),
-                    child: IconButton(
-                      icon: const Icon(Icons.photo_camera, size: 70),
-                      color: Colors.blue,
-                      onPressed: () {
-                        // TODO: implement photo upload
-                      },
+                    padding: const EdgeInsets.fromLTRB(16.0, 10.0, 16.0, 16.0),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        TextFormField(
+                          decoration: const InputDecoration(labelText: 'Name'),
+                          initialValue: _name,
+                          onChanged: (value) {
+                            setState(() {
+                              _name = value;
+                            });
+                          },
+                        ),
+                        const SizedBox(height: 16.0),
+                        TextFormField(
+                          decoration:
+                              const InputDecoration(labelText: 'Last Name'),
+                          initialValue: _lastName,
+                          onChanged: (value) {
+                            setState(() {
+                              _lastName = value;
+                            });
+                          },
+                        ),
+                        const SizedBox(height: 16.0),
+                        TextFormField(
+                          decoration: const InputDecoration(labelText: 'Email'),
+                          initialValue: _email,
+                          onChanged: (value) {
+                            setState(() {
+                              _email = value;
+                            });
+                          },
+                        ),
+                        const SizedBox(height: 16.0),
+                        TextFormField(
+                          decoration:
+                              const InputDecoration(labelText: 'Telephone'),
+                          initialValue: _telephone,
+                          onChanged: (value) {
+                            setState(() {
+                              _telephone = value;
+                            });
+                          },
+                        ),
+                        const SizedBox(height: 16.0),
+                        TextFormField(
+                          decoration:
+                              const InputDecoration(labelText: 'Weight'),
+                          initialValue: _weight?.toString(),
+                          keyboardType: const TextInputType.numberWithOptions(
+                              decimal: true),
+                          onChanged: (value) {
+                            setState(() {
+                              _weight = double.tryParse(value);
+                            });
+                          },
+                        ),
+                        const SizedBox(height: 16.0),
+                        TextFormField(
+                          decoration:
+                              const InputDecoration(labelText: 'Height'),
+                          initialValue: _height?.toString(),
+                          keyboardType: const TextInputType.numberWithOptions(
+                              decimal: true),
+                          onChanged: (value) {
+                            setState(() {
+                              _height = double.tryParse(value);
+                            });
+                          },
+                        ),
+                      ],
                     ),
                   ),
                 ],
               ),
-              const SizedBox(height: 20.0),
-              Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(16.0),
-                ),
-                padding: const EdgeInsets.fromLTRB(16.0, 10.0, 16.0, 16.0),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    TextFormField(
-                      decoration: const InputDecoration(labelText: 'Name'),
-                      initialValue: _name,
-                      onChanged: (value) {
-                        setState(() {
-                          _name = value;
-                        });
-                      },
-                    ),
-                    const SizedBox(height: 16.0),
-                    TextFormField(
-                      decoration: const InputDecoration(labelText: 'Last Name'),
-                      initialValue: _lastName,
-                      onChanged: (value) {
-                        setState(() {
-                          _lastName = value;
-                        });
-                      },
-                    ),
-                    const SizedBox(height: 16.0),
-                    TextFormField(
-                      decoration: const InputDecoration(labelText: 'Email'),
-                      initialValue: _email,
-                      onChanged: (value) {
-                        setState(() {
-                          _email = value;
-                        });
-                      },
-                    ),
-                    const SizedBox(height: 16.0),
-                    TextFormField(
-                      decoration: const InputDecoration(labelText: 'Telephone'),
-                      initialValue: _telephone,
-                      onChanged: (value) {
-                        setState(() {
-                          _telephone = value;
-                        });
-                      },
-                    ),
-                    const SizedBox(height: 16.0),
-                    TextFormField(
-                      decoration: const InputDecoration(labelText: 'Weight'),
-                      initialValue: _weight?.toString(),
-                      keyboardType:
-                          const TextInputType.numberWithOptions(decimal: true),
-                      onChanged: (value) {
-                        setState(() {
-                          _weight = double.tryParse(value);
-                        });
-                      },
-                    ),
-                    const SizedBox(height: 16.0),
-                    TextFormField(
-                      decoration: const InputDecoration(labelText: 'Height'),
-                      initialValue: _height?.toString(),
-                      keyboardType:
-                          const TextInputType.numberWithOptions(decimal: true),
-                      onChanged: (value) {
-                        setState(() {
-                          _height = double.tryParse(value);
-                        });
-                      },
-                    ),
-                  ],
-                ),
-              ),
-            ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
